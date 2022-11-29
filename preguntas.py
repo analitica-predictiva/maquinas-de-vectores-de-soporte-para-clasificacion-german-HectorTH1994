e23fa19a6db8adbebf8df4301b9b330cfba1045f
Hector David Taticuan <105394300+HectorTH1994@users.noreply.github.com>
2022-11-28 20:34:26 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -231,7 +231,7 @@ def pregunta_03():
     )
 
     # Entrene el pipeline con los datos de entrenamiento.
-    pipeline.fit(x_train, y_train)
+    pipeline.fit(X_train, y_train)
 
     # # Retorne el pipeline entrenado
     return pipeline
